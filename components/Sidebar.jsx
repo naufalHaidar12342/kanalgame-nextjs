fad831c1bed2cd33aeeb5f872959f56984b7df47
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-08-07 23:03:01 +0700

    menambahkan pages dan navbar pada index

---
+++ components/Sidebar.jsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+	return (
+		<div>
+			<div className="text-2xl text-red-600">
+				<p>Deserunt exercitation voluptate cillum anim sint.</p>
+			</div>
+		</div>
+	);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./pages/**/*.{js,jsx}"],
+	content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				Keppel: "#58B09C",
+				HunterGreen: "#386150",
+				AeroBlue: "#CAF7E2",
+				OliveDrab7: "#4A442D",
+				OliveDrab7Variant: "#3D3522",
+			},
+		},
 	},
 	plugins: [],
 };
